6be489d64229e6abafbf1c67e29b89cf6e7d3596
SirGeoffers <geoffersthethird@gmail.com>
2020-01-04 17:03:46 -0500

    initial commit

---
+++ Assets/Scripts/LedStripController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LedStripController : MonoBehaviour
+{
+    [SerializeField]
+    private MeshRenderer[] leds = null;
+}
